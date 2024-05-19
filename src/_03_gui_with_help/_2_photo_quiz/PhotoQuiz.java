4f046df158eb184b15d193ddac6bae2fa987089c
UzPC <UzPC@UZLAP.home>
2024-05-19 12:54:49 -0700

    DrumKit in progress

--- src/_03_gui_with_help/_2_photo_quiz/PhotoQuiz.java
+++ src/_03_gui_with_help/_2_photo_quiz/PhotoQuiz.java
@@ -32,29 +32,39 @@ public class PhotoQuiz {
 		// 3. use the "createImage()" method below to initialize your Component
 
 		// 4. add the image to the quiz window
-		
+		quizWindow.add(component);
 		// 5. call the pack() method on the quiz window
-
+		quizWindow.pack();
 		// 6. ask a question that relates to the image
-
+		String question = JOptionPane.showInputDialog("How many trophies are in the image");
 		// 7. print "CORRECT" if the user gave the right answer
-
+		if(question.equals("17")) {
+			JOptionPane.showMessageDialog(null, "Correct!");
+		}
 		// 8. print "INCORRECT" if the answer is wrong
-
+		else {
+			JOptionPane.showMessageDialog(null, "Incorrect!");
+		}
 		// 9. remove the component from the quiz window (you may not see the
 		// effect of this until step 12)
-
+		quizWindow.remove(component);
 		// 10. find another image and create it (might take more than one line
 		// of code)
-
+		image = ("https://img.freepik.com/free-photo/painting-mountain-lake-with-mountain-background_188544-9126.jpg");
 		// 11. add the second image to the quiz window
-
+		component = createImage(image);
 		// 12. pack the quiz window
-
+		quizWindow.add(component);
+		quizWindow.pack();
 		// 13. ask another question
-
+		question = JOptionPane.showInputDialog("Is the weather in the image pleasent and calm or is it rough and stormy?");
 		// 14+ check answer, say if correct or incorrect, etc.
-
+		if(question.equals("pleasent and calm")) {
+			JOptionPane.showMessageDialog(null, "Correct!");
+		}
+		else {
+			JOptionPane.showMessageDialog(null, "Incorrect!");
+		}
 	}
 
 	private Component createImage(String imageUrl) {
